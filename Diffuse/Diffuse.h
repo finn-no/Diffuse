98556e3ddb181408ddf844539379911292168fa9
Bastian Stien <bastian.stien@finn.no>
2018-11-07 09:59:00 +0100

    Initial Commit

---
+++ Diffuse/Diffuse.h
@@ -0,0 +1,11 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for Diffuse.
+FOUNDATION_EXPORT double DiffuseVersionNumber;
+
+//! Project version string for Diffuse.
+FOUNDATION_EXPORT const unsigned char DiffuseVersionString[];
