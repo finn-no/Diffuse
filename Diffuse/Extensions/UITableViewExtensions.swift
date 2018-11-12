ce444fa55b2ad8af0418bb31ca4fb4df55c5ffdc
Bastian Stien <bastian.stien@finn.no>
2018-11-12 11:23:38 +0100

    Specify animations within a dict when reloading UITableView from diff

--- Diffuse/Extensions/UITableViewExtensions.swift
+++ Diffuse/Extensions/UITableViewExtensions.swift
@@ -4,15 +4,23 @@
 
 import UIKit
 
+public enum TableViewOperation {
+    case insert
+    case reload
+    case delete
+}
+
 extension UITableView {
     public func reload(with changes: CollectionChanges,
-                       insertAnimation: UITableView.RowAnimation = .automatic,
-                       reloadAnimation: UITableView.RowAnimation = .automatic,
-                       deleteAnimation: UITableView.RowAnimation = .automatic,
+                       animations: [TableViewOperation: UITableView.RowAnimation]?,
                        section: Int = 0,
                        updateDataSource: () -> Void) {
         guard changes.allChanges.count != 0 else { return }
         let indexPaths = IndexPathResult(changes: changes, section: section)
+
+        let insertAnimation = animations?[.insert] ?? .automatic
+        let reloadAnimation = animations?[.reload] ?? .automatic
+        let deleteAnimation = animations?[.delete] ?? .automatic
 
         if #available(iOS 11, *) {
             performBatchUpdates({
