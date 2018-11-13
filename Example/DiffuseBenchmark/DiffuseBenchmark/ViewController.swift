0f3d98cdd55b0cbc173cc60b3b11bb86290fa44a
Vadym Markov <markov.vadym@gmail.com>
2018-11-13 13:09:03 +0100

    Use average for comparison

--- Example/DiffuseBenchmark/DiffuseBenchmark/ViewController.swift
+++ Example/DiffuseBenchmark/DiffuseBenchmark/ViewController.swift
@@ -11,6 +11,8 @@ final class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
+        print("Start...")
+
         runBenchmarks(count: 10)
         runBenchmarks(count: 100)
         runBenchmarks(count: 1000)
@@ -41,27 +43,20 @@ final class ViewController: UIViewController {
     }
 
     private func runBenchmarks(old: [Int], new: [Int]) {
-        print("Benchmark: \(old.count) old, \(new.count) new elements\n")
+        print("\nBenchmark: \(old.count) old, \(new.count) new elements\n")
 
-        var benchmarks = [Benchmark]()
+        let benchmarks = Algorithm.allCases.map(Benchmark.init)
 
-        benchmarks.append(Benchmark(name: "Diffuse old", execute: {
-            _ = Diffuse.diff(old: old, new: new)
-        }))
-
-        benchmarks.append(Benchmark(name: "Differific", execute: {
-            _ = DiffManager().diff(old, new)
-        }))
-
-        benchmarks.append(Benchmark(name: "DeepDiff", execute: {
-            _ = DeepDiff.diff(old: old, new: new)
-        }))
-
-        for (offset, benchmark) in benchmarks.sorted(by: { $0.timeInterval < $1.timeInterval }).enumerated() {
-            print("\(offset + 1). \(benchmark.name): \(benchmark.timeInterval)s")
+        // Call the diff function of every algorithm with the same input 10 times and calculate the average.
+        for _ in 0...10 {
+            for benchmark in benchmarks {
+                benchmark.peformDiff(old: old, new: new)
+            }
         }
 
-        print("\n")
+        for (offset, benchmark) in benchmarks.sorted(by: { $0.average < $1.average }).enumerated() {
+            print("\(offset + 1). \(benchmark.algorithm.rawValue): \(benchmark.average)s")
+        }
     }
 
     private func generate(count: Int) -> (old: [Int], new: [Int]) {
@@ -83,15 +78,52 @@ final class ViewController: UIViewController {
 
 // MARK: - Custom types
 
-private struct Benchmark {
-    let name: String
-    let timeInterval: TimeInterval
+private enum Algorithm: String, CaseIterable {
+    case diffuse = "Diffuse"
+    case differific = "Differific"
+    case deepDiff = "DeepDiff"
 
-    init(name: String, execute: () -> Void) {
-        self.name = name
+    func diffFunction(old: [Int], new: [Int]) -> () -> Void {
+        switch self {
+        case .diffuse:
+            return { _ = Diffuse.diff(old: old, new: new) }
+        case .differific:
+            return { _ = DiffManager().diff(old, new) }
+        case .deepDiff:
+            return { _ = DeepDiff.diff(old: old, new: new) }
+        }
+    }
+}
+
+private final class Benchmark {
+    let algorithm: Algorithm
+    private var timeIntervals = [TimeInterval]()
+
+    var average: TimeInterval {
+        return timeIntervals.average
+    }
+
+    init(algorithm: Algorithm) {
+        self.algorithm = algorithm
+    }
+
+    func peformDiff(old: [Int], new: [Int]) {
         let start = Date()
-        execute()
+        algorithm.diffFunction(old: old, new: new)()
         let end = Date()
-        timeInterval =  end.timeIntervalSince1970 - start.timeIntervalSince1970
+        let timeInterval = end.timeIntervalSince1970 - start.timeIntervalSince1970
+        timeIntervals.append(timeInterval)
+    }
+}
+
+// MARK: - Private extensions
+
+private extension Collection where Element: Numeric {
+    var total: Element { return reduce(0, +) }
+}
+
+private extension Collection where Element: BinaryFloatingPoint {
+    var average: Element {
+        return isEmpty ? 0 : total / Element(count)
     }
 }
