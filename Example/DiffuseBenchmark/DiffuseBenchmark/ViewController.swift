fa1b7372831cc26721cf5d9b295ad077f36f1e9c
Henrik Brustad <henrikbrustad@gmail.com>
2018-11-16 13:06:12 +0100

    Clean up benchmark project

--- Example/DiffuseBenchmark/DiffuseBenchmark/ViewController.swift
+++ Example/DiffuseBenchmark/DiffuseBenchmark/ViewController.swift
@@ -80,7 +80,6 @@ final class ViewController: UIViewController {
 
 private enum Algorithm: String, CaseIterable {
     case diffuse = "Diffuse"
-    case diffuse = "Diffuse"
     case differific = "Differific"
     case deepDiff = "DeepDiff"
 
@@ -88,8 +87,6 @@ private enum Algorithm: String, CaseIterable {
         switch self {
         case .diffuse:
             return { _ = diff(old: old, new: new) }
-        case .diffuse2:
-            return { _ = diff2(old: old, new: new) }
         case .differific:
             return { _ = DiffManager().diff(old, new) }
         case .deepDiff:
@@ -130,160 +127,3 @@ private extension Collection where Element: BinaryFloatingPoint {
         return isEmpty ? 0 : total / Element(count)
     }
 }
-
-public func diff<T: Hashable>(old: [T], new: [T]) -> CollectionChanges {
-    // 1 - We can return early in some cases
-    if old.isEmpty { return CollectionChanges(inserted: Array(0..<new.count)) }
-    if new.isEmpty { return CollectionChanges(removed: Array(0..<old.count)) }
-
-    // This is useful when reserving memory
-    let minSize = min(old.count, new.count)
-
-    // Setup the arrays for all the operations
-    var inserted = Set<Int>(minimumCapacity: new.count) // Make inserted a set for now, makes it faster to find updated elements
-    var removed = [Int]()
-    var moved = [Move<Int>]()
-    var updated = [Int]()
-
-    // We can't remove more items than the amount of element in 'old'
-    removed.reserveCapacity(old.count)
-    moved.reserveCapacity(minSize)
-    updated.reserveCapacity(minSize)
-
-    // 2 - Skip all the equal elements in the beginning of the arrays
-    var startIndex = 0
-    for i in 0..<minSize {
-        if new[i] != old[i] {
-            // These are the first two elements which are different
-            startIndex = i
-            break
-        }
-    }
-
-    // 3 - Make the old array a set for fast lookup on values
-    var oldSet = Set<Element<T>>(minimumCapacity: old.count)
-    for i in startIndex..<old.count {
-        oldSet.insert(Element(value: old[i], index: i))
-    }
-
-    // 4 - Iterate the 'new' array and compare against elements in 'oldSet'
-    for i in startIndex ..< new.count {
-        // 4.1 - Need to create elements to compare elements in the 'oldSet'
-        let element = Element(value: new[i], index: i)
-
-        // 4.2 - Search for 'element' in the 'oldSet'
-        if let movedElement = oldSet.remove(element) {
-            // If we find 'element' in the 'oldSet', it could have been moved
-            // 'element' has only moved if the index has changed
-            if movedElement.index != i {
-                // The indeces are different so 'element' has moved
-                moved.append((from: movedElement.index, to: element.index))
-            }
-        } else {
-            // If 'element' is not in 'oldSet' is has been inserted into the 'new' array
-            inserted.insert(i)
-        }
-    }
-
-    // 5 - Find elements that are updated
-    for element in oldSet {
-        if let update = inserted.remove(element.index) {
-            // If we find 'element.index' in 'inserted', there has been a remove and insert at the same index
-            // and we can consider 'element' as updated
-            updated.append(update)
-        } else {
-            // If 'element.index' is not in 'inserted' it has been removed from 'old'
-            removed.append(element.index)
-        }
-    }
-
-    // 6 - Convert 'inserted' to an array and return with the other operations
-    return CollectionChanges(inserted: Array<Int>(inserted), removed: removed, moved: moved, updated: updated)
-}
-
-public func diff2<T: Hashable>(old: [T], new: [T]) -> CollectionChanges {
-    // 1 - We can return early in some cases
-    if old.isEmpty { return CollectionChanges(inserted: Array(0..<new.count)) }
-    if new.isEmpty { return CollectionChanges(removed: Array(0..<old.count)) }
-
-    // This is useful when reserving memory
-    let minSize = min(old.count, new.count)
-
-    // Setup the arrays for all the operations
-    var inserted = Set<Int>(minimumCapacity: new.count) // Make inserted a set for now, makes it faster to find updated elements
-    var removed = [Int]()
-    var moved = [Move<Int>]()
-    var updated = [Int]()
-
-    // We can't remove more items than the amount of element in 'old'
-    removed.reserveCapacity(old.count)
-    moved.reserveCapacity(minSize)
-    updated.reserveCapacity(minSize)
-
-    // 2 - Skip all the equal elements in the beginning of the arrays
-    var startIndex = 0
-    for i in 0..<minSize {
-        if new[i] != old[i] {
-            // These are the first two elements which are different
-            startIndex = i
-            break
-        }
-    }
-
-    // 3 - Make the old array a set for fast lookup on values
-    var oldSet = Set<Element<T>>(minimumCapacity: old.count)
-    for i in startIndex..<old.count {
-        oldSet.insert(Element(value: old[i], index: i))
-    }
-
-    // 4 - Iterate the 'new' array and compare against elements in 'oldSet'
-    for i in startIndex ..< new.count {
-        // 4.1 - Need to create elements to compare elements in the 'oldSet'
-        let element = Element(value: new[i], index: i)
-
-        // 4.2 - Search for 'element' in the 'oldSet'
-        if let movedElement = oldSet.remove(element) {
-            // If we find 'element' in the 'oldSet', it could have been moved
-            // 'element' has only moved if the index has changed
-            if movedElement.index != i {
-                // The indeces are different so 'element' has moved
-                moved.append((from: movedElement.index, to: element.index))
-            }
-        } else {
-            // If 'element' is not in 'oldSet' is has been inserted into the 'new' array
-            inserted.insert(i)
-        }
-    }
-
-    // 5 - Find elements that are updated
-    for element in oldSet {
-        if let update = inserted.remove(element.index) {
-            // If we find 'element.index' in 'inserted', there has been a remove and insert at the same index
-            // and we can consider 'element' as updated
-            updated.append(update)
-        } else {
-            // If 'element.index' is not in 'inserted' it has been removed from 'old'
-            removed.append(element.index)
-        }
-    }
-
-    // 6 - Convert 'inserted' to an array and return with the other operations
-    return CollectionChanges(inserted: Array<Int>(inserted), removed: removed, moved: moved, updated: updated)
-}
-
-
-struct Element<T: Hashable>: Hashable {
-    let value: T
-    let index: Int
-
-    var hashValue: Int {
-        return value.hashValue
-    }
-
-    static func == (lhs: Element<T>, rhs: Element<T>) -> Bool {
-        return lhs.value == rhs.value
-    }
-}
-
-public typealias Move<T> = (from: T, to: T)
-
